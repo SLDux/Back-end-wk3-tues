ec333244dc1a8ed9780bd12adc18183109bf9472
SLDux <47551368+SLDux@users.noreply.github.com>
2019-02-26 14:31:01 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html> 
+
+    <head>
+        <title>Test</title>
+    </head>
+    <body> 
+        
+        <?php echo "<h1>hello world</h1>" ?> 
+        <?php echo "<ul><li>1</li><li>2</li><li>3</li></ul>" ?> 
+        <?php echo "<p>This is a paragraph. Apparently you can put html inside php brackets, but i notice brackets is not autofilling it, so i'll have to watch out for errors in my code. Also, what is the difference between echo and print? i'm currently using echo.</p>" ?>
+        <?php print "this is print, i'm not using html paragraph tags for this one. it's just print." ?>
+        
+        <?php 
+            // this is a test
+            echo "hello world -is this the usual set up?"
+        ?>
+        
+    </body>
+
+</html>
